6cebd98f624de3c181cbbbcfb074502b51ba9701
dhennigar <danrhennigar@gmail.com>
2023-08-28 20:49:32 -0700

    enable the swank server

--- init.lisp
+++ init.lisp
@@ -9,33 +9,29 @@
 (in-package :stumpwm)
 
 ;; -----------------------------------------------------------------------------
-;; general settings
+;; General
 
 (setf
  *mouse-focus-policy* :click
  *run-or-raise-all-groups* t   ; search across groups for run-or-raise
  *run-or-raise-all-screens* t) ; search across screens for run-or-raise
 
-(set-prefix-key (kbd "C-z"))
-
 
 ;; -----------------------------------------------------------------------------
-;; Launch applications
+;; Application shortcuts
 
 ;; Jump to Chrome if it exists, else launch it.
-(defcommand chrome ()()
-  (run-or-raise "flatpak run --file-forwarding com.google.Chrome" '(:class "Google-chrome")))
+(defcommand chrome
+    ()()
+    (run-or-raise "flatpak run --file-forwarding com.google.Chrome"
+		  '(:class "Google-chrome")))
 (define-key *root-map* (kbd "b") "chrome")
-
-(define-key *top-map* (kbd "M-Tab") "next")
-(define-key *top-map* (kbd "M-S-Tab") "prev")
 
 
 ;; -----------------------------------------------------------------------------
 ;; Fonts
-;; Note: TrueType fonts are basically not supported at all.
 
-(ql:quickload :clx-truetype)
+(ql:quickload :clx-truetype) ; note that clx-truetype must be obtained from a repo
 (load-module "ttf-fonts")
 (set-font (make-instance 'xft:font :family "Noto Mono"
 			 :subfamily "Regular" :size 10))
@@ -44,39 +40,55 @@
 ;; -----------------------------------------------------------------------------
 ;; Mode-line
 
-(setf *group-format* "%t")
-(setf *window-format "%n: %20t")
+(load-module "battery-portable") ; provides the %B formatter
+
+(setf *window-format* "%n:%s%c")
 (setf *time-modeline-string* "%k:%M")
-
-;(setf *screen-mode-line-format*
-;    (list "%g | %v ^>" '(:eval (run-shell-command "date '+%k:%M'" t))))
-
 (setf *screen-mode-line-format*
-      (list "%n: %v ^> %d"))
+      (list "%n: " "%v" "^> %B %d"))
 
 (stumpwm:toggle-mode-line (stumpwm:current-screen)
 			  (stumpwm:current-head))
 
 
 ;; -----------------------------------------------------------------------------
+;; Keybindings
+
+(set-prefix-key (kbd "C-z"))
+
+;; Top-level switching
+(define-key *top-map* (kbd "M-Tab") "pull-hidden-next")
+(define-key *top-map* (kbd "M-S-Tab") "pull-hidden-prev")
+(define-key *top-map* (kbd "C-M-Tab") "gnext")
+(define-key *top-map* (kbd "C-M-S-Tab") "gprev")
+
+;; Windows Compat
+(define-key *top-map* (kbd "C-s-d") "gnew")
+(define-key *top-map* (kbd "C-s-Right") "gnext")
+(define-key *top-map* (kbd "C-s-Left") "gprev")
+
+;; Media
+(define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec pamixer -i 2")
+(define-key *top-map* (kbd "XF86AudioLowerVolume") "exec pamixer -d 2")
+(define-key *top-map* (kbd "XF86AudioMute")        "exec pamixer -t")
+
+;; -----------------------------------------------------------------------------
 ;; Groups
 
+;; Default groups
 (when *initializing*
-  (grename "Workspace 1")
-  (gnew "Workspace 2")
-  (gnew "Workspace 3")
-  (gnew "Emacs")
-  (gselect "Workspace 1"))
+  (grename "Group1")
+  (gnew "Group2")
+  (gnew "Group3")
+  (gselect "Group1"))
 
 
 ;; -----------------------------------------------------------------------------
-;; Window placement rules
+;; Swank
 
-(clear-window-placement-rules) ; Clear all window placement rules
+;; Start a server to communicate with Emacs
+(require 'swank)
+(swank:create-server :port 4004)
 
-;; Place emacs in its own group and jump to it
-(define-frame-preference "Emacs" (1 t t :class "Emacs"))
-
-;; I will probably want to configure R's graphics windows here.
 
 ;;; init.lisp ends here
