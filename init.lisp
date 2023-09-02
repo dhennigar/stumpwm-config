4d6285945c11de994fe2a60d94f818b5ca356807
dhennigar <danrhennigar@gmail.com>
2023-09-01 18:06:15 -0700

    updates

--- init.lisp
+++ init.lisp
@@ -9,9 +9,18 @@
 (in-package :stumpwm)
 
 ;; -----------------------------------------------------------------------------
+;; X initialization
+
+(run-shell-command "xsetroot -cursor_name left_ptr")
+(run-shell-command "xrdb -merge .Xresources")
+
+
+;; -----------------------------------------------------------------------------
 ;; General
 
 (setf
+ *message-window-gravity* :center
+ *input-window-gravity* :center
  *mouse-focus-policy* :click
  *run-or-raise-all-groups* t   ; search across groups for run-or-raise
  *run-or-raise-all-screens* t) ; search across screens for run-or-raise
@@ -25,7 +34,7 @@
     ()()
     (run-or-raise "flatpak run --file-forwarding com.google.Chrome"
 		  '(:class "Google-chrome")))
-(define-key *root-map* (kbd "b") "chrome")
+(define-key *root-map* (kbd "w") "chrome")
 
 
 ;; -----------------------------------------------------------------------------
@@ -42,8 +51,14 @@
 
 (load-module "battery-portable") ; provides the %B formatter
 
-(setf *window-format* "%n:%s%c")
-(setf *time-modeline-string* "%k:%M")
+
+(setf *window-format* "%n:%s%c"
+      *time-modeline-string* "%k:%M"
+      *mode-line-position* :bottom
+      *mode-line-border-width* 0
+      *mode-line-pad-x* 5
+      *mode-line-pad-y* 2
+      *mode-line-background-color* 'black)
 (setf *screen-mode-line-format*
       (list "%n: " "%v" "^> %B %d"))
 
@@ -56,31 +71,40 @@
 
 (set-prefix-key (kbd "C-z"))
 
-;; Top-level switching
-(define-key *top-map* (kbd "M-Tab") "pull-hidden-next")
-(define-key *top-map* (kbd "M-S-Tab") "pull-hidden-prev")
-(define-key *top-map* (kbd "C-M-Tab") "gnext")
-(define-key *top-map* (kbd "C-M-S-Tab") "gprev")
+;; remove redundant bindings
+(undefine-key *root-map* (kbd "s"))
+(undefine-key *root-map* (kbd "S"))
 
-;; Windows Compat
-(define-key *top-map* (kbd "C-s-d") "gnew")
-(define-key *top-map* (kbd "C-s-Right") "gnext")
-(define-key *top-map* (kbd "C-s-Left") "gprev")
+;; emacs-like navigation
+(define-key *root-map* (kbd "b") "select-window")
+(define-key *root-map* (kbd "0") "remove-split")
+(define-key *root-map* (kbd "1") "only")
+(define-key *root-map* (kbd "2") "vsplit")
+(define-key *root-map* (kbd "3") "hsplit")
+
+;; no need for an exchange map
+(undefine-key *root-map* (kbd "x"))
+(setf *exchange-window-map* nil)
+(define-key *root-map* (kbd "S-Left") "exchange-direction left")
+(define-key *root-map* (kbd "S-Right") "exchange-direction right")
+(define-key *root-map* (kbd "S-Up") "exchange-direction up")
+(define-key *root-map* (kbd "S-Down") "exchange-direction down")
 
 ;; Media
 (define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec pamixer -i 2")
 (define-key *top-map* (kbd "XF86AudioLowerVolume") "exec pamixer -d 2")
 (define-key *top-map* (kbd "XF86AudioMute")        "exec pamixer -t")
 
+
 ;; -----------------------------------------------------------------------------
 ;; Groups
 
 ;; Default groups
 (when *initializing*
-  (grename "Group 1")
-  (gnew "Group 2")
-  (gnew "Group 3")
-  (gselect "Group 1"))
+	(setf (group-name (car (screen-groups (current-screen))))
+      		"Group1")
+	(gnewbg "Group2")
+	(gnewbg "Group3"))
 
 
 ;; -----------------------------------------------------------------------------
@@ -88,12 +112,13 @@
 
 (require 'swank)
 
+(swank-loader:init)
+
 (defcommand swank ()()
 	"Start a swank server."
 	(message "Swank server started on port 4004")
-	(swank:create-server :port 4004))
-
-(define-key *root-map* (kbd "C-s") "swank")
-
+  (swank:create-server :port 4004
+		       :style swank:*communication-style*
+		       :dont-close t))
 
 ;;; init.lisp ends here
